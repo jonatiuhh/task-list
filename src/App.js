ef833b279b14ea3ba072f0ebc9a0d98bdc434f73
Jonathan Sesma <108715533+jonatiuhh@users.noreply.github.com>
2022-08-04 16:02:22 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import ListaDeTareas from './componentes/listadetareas';
+import meme from './images/1.jpg';
+import hand from './images/hand.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <h2 className='text-1'>Se te olvidan tus pendientes?</h2>
+      <div className='meme-contenedor'>
+        <img
+        src={meme}
+        className='meme'
+        alt='mememe' 
+        />
+      </div>
+      <h2 className='text-2'>Crea una lista de tareas</h2>
+      <div className='hand-contenedor'>
+        <img 
+        src={hand} 
+        className='hand'
+        alt='hand'
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
